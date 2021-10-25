cc3d4925ab02b74f8bef842e769e70556bfadf38
Leonardo Monti <contatoleomonti@gmail.com>
2021-10-25 16:07:40 -0300

    corrige lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Home }/>
+        <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
   );

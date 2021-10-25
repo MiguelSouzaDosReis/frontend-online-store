8b0008f2a3806122a1910cdefe3bf84ceb874c4b
Leonardo Monti <contatoleomonti@gmail.com>
2021-10-25 17:23:09 -0300

    adiciona requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.requiredCategories();
+  }
+
+  async requiredCategories() {
+    const response = await getCategories();
+    this.setState({
+      categories: [...response],
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
-        <label htmlFor="input">
-          <input id="input" type="text" />
-        </label>
-        <Link to="/cart" data-testid="shopping-cart-button">Carrinho de Compras</Link>
-        <p
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <aside>
+          <ul>
+            {categories.map((category) => (
+              <li key={ category.id }>
+                <label htmlFor={ category.id }>
+                  <input
+                    data-testid="category"
+                    id={ category.id }
+                    type="radio"
+                  />
+                  {category.name}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </aside>
+        <div>
+          <label htmlFor="input">
+            <input id="input" type="text" />
+          </label>
+          <Link to="/cart" data-testid="shopping-cart-button">Carrinho de Compras</Link>
+          <p
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
       </div>
+
     );
   }
 }

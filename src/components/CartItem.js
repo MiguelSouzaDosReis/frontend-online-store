050f18f49c90dceef76a28011423c6cdde922cd2
Danilo <dcalado.fisio@yahoo.com.br>
2021-10-28 12:41:23 -0300

    adicionando produtos em grid e inciando css do carrinho com itens e sem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,15 +10,15 @@ class CartItem extends Component {
     };
   }
 
-  handleIncrease = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  }
-
   handleDecrease = () => {
     this.setState((prevState) => ({
       count: prevState.count - 1,
+    }));
+  }
+
+  handleIncrease = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
     }));
   }
 
@@ -26,29 +26,34 @@ class CartItem extends Component {
     const { count } = this.state;
     const { product } = this.props;
     return (
-      <>
-        <div key={ product.id }>
+      <div className="cart-itens-div">
+        <div className="cart-itens-div1" key={ product.id }>
           <p data-testid="shopping-cart-product-name">{ product.title }</p>
           <p>{ product.id }</p>
           <img src={ product.thumbnail } alt={ product.title } />
-          <p>{ product.price }</p>
         </div>
         <button
-          data-testid="product-increase-quantity"
-          type="button"
-          onClick={ this.handleIncrease }
-        >
-          +
-        </button>
-        <p data-testid="shopping-cart-product-quantity">{ count }</p>
-        <button
+          className="decrease-button"
           data-testid="product-decrease-quantity"
           type="button"
           onClick={ count > 0 && this.handleDecrease }
         >
           -
         </button>
-      </>
+        <div className="quantity">
+          <span>Quantidade</span>
+          <h2 size="20px" data-testid="shopping-cart-product-quantity">{ count }</h2>
+        </div>
+        <button
+          className="increase-button"
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ this.handleIncrease }
+        >
+          +
+        </button>
+        <h1>{ product.price }</h1>
+      </div>
     );
   }
 }
